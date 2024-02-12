b20e8f94b833ac48c35187020f38ef9c866cb35c
johnnysedh3lllo <johnnysaidh3lllo@gmail.com>
2024-02-12 12:50:59 +0100

    implemented client side validation using the Constraint Validation API

--- src/script.js
+++ src/script.js
@@ -1,5 +1,43 @@
 "use strict";
+const form = document.querySelector(".main__form");
+const emailInput = document.getElementById("mail");
+const emailError = document.querySelector(".error");
 
-//main__form
-//main__form__input
-//main__form__btn
+function showError() {
+  if (emailInput.validity.valueMissing) {
+    emailError.textContent = "Please provide a valid email address";
+  } else if (emailInput.validity.typeMismatch) {
+    emailError.textContent = "Value needs to be an email address";
+  }
+  emailError.className = "error active";
+  document.querySelector(".error-icon").style.display = "inline";
+}
+
+function clearError() {
+  emailError.textContent = "";
+  emailError.className = "error";
+  document.querySelector(".error-icon").style.display = "none";
+}
+
+emailInput.addEventListener("input", function () {
+  if (emailInput.validity.valid) {
+    emailError.textContent = "";
+    emailError.className = "error";
+    document.querySelector(".error-icon").style.display = "none";
+  } else {
+    showError();
+  }
+});
+
+form.addEventListener("submit", function (event) {
+  if (!email.validity.valid) {
+    showError();
+    event.preventDefault();
+  }
+});
+
+document.body.addEventListener("click", function (event) {
+  if (event) {
+    clearError();
+  }
+});
